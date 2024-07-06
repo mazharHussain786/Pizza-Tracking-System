c6f30eea3fbb615ae1d34b9f56d4e17c5c35c2f3
mazharHussain786 <mianmazhar301@gmail.com>
2024-07-06 09:25:51 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
+
+const app = express();
+
+const Port = process.env.Port || 3000;
+
+app.get("/", (req, res) => {
+  res.render("home"); // we have already tell the path in below
+});
+
+app.use(expressLayout);
+
+app.set("views", path.join(__dirname, "/resources/views")); //isne view ka adress btaya
+
+app.set("view engine", 'ejs'); // isne btaya k view_engine ejs ko use kry gaa
+
+app.listen(Port, () => {
+  console.log(`http://localhost:${Port}`);
+});
